2ee9334a9e94ef57038e2f11a6bc8c0bc039649a
Zhanna <zhanna.libman@gmail.com>
2017-01-15 17:18:34 +0200

    added opener

--- app.js
+++ app.js
@@ -5,5 +5,5 @@
 const shelljs = require('shelljs');
 shelljs.exec("node_modules/.bin/http-server", {async: true});
 var spawn = require('child_process').spawn;
-spawn('open', ['http://127.0.0.1:8080']);
+spawn('open', ['http://127.0.0.1:8080/index.html']);
 
