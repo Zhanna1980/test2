0e89e35b516386e597ffdede2cd19f2a900e0d91
Zhanna <zhanna.libman@gmail.com>
2017-01-15 17:11:17 +0200

    third commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
  */
 
 const shelljs = require('shelljs');
-shelljs.exec("../node_modules/.bin/http-server", {async: true});
+shelljs.exec("node_modules/.bin/http-server", {async: true});
+var spawn = require('child_process').spawn;
+spawn('open', ['http://127.0.0.1:8080']);
 
-console.log("xxx");
